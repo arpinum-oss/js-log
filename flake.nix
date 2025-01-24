5fdf1772d214ff67c86b654732c5b71090981d69
Michael Borde <michael.borde@ubimail.fr>
2025-01-24 15:46:55 +0100

    build: using nix

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "js-config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [ nodejs_20 ];
+        };
+      }
+    );
+ }
